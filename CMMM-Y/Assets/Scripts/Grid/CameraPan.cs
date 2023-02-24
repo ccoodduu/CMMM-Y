31d72ecfc474444e50ff38cec498c94c6a0a1adc
ccoodduu <william@guldagermadsen.dk>
2023-02-24 11:38:35 +0100

    began work on new zoom

--- CMMM-Y/Assets/Scripts/Grid/CameraPan.cs
+++ CMMM-Y/Assets/Scripts/Grid/CameraPan.cs
@@ -4,46 +4,63 @@ using UnityEngine;
 
 public class CameraPan : MonoBehaviour
 {
-    private float orthographicSize;
-    private float x;
-    private float y;
+	private float orthographicSize;
+	private float x;
+	private float y;
 
-    private void Start()
-    {
-        instance = this;
-    }
+	private void Start()
+	{
+		instance = this;
+	}
 
-    public void PositionCamera()
-    {
-        this.x = (float)CellFunctions.gridWidth * 0.5f - 0.5f;
-        this.y = (float)CellFunctions.gridHeight * 0.5f - 0.5f;
-        this.orthographicSize = (float)CellFunctions.gridHeight * 0.5f + 2f;
-        base.transform.position = new Vector3(this.x, this.y, -10f);
-        base.GetComponent<Camera>().orthographicSize = this.orthographicSize;
-    }
+	public void PositionCamera()
+	{
+		this.x = (float)CellFunctions.gridWidth * 0.5f - 0.5f;
+		this.y = (float)CellFunctions.gridHeight * 0.5f - 0.5f;
+		this.orthographicSize = (float)CellFunctions.gridHeight * 0.5f + 2f;
+		base.transform.position = new Vector3(this.x, this.y, -10f);
+		base.GetComponent<Camera>().orthographicSize = this.orthographicSize;
+	}
 
-    public void Update()
-    {
-        this.orthographicSize -= Input.mouseScrollDelta.y * 0.5f * Mathf.Sqrt(this.orthographicSize);
-        if (this.orthographicSize < 0.5)
-            this.orthographicSize = 0.5f;
+	public void Update()
+	{
+		Zoom(Input.mouseScrollDelta.y * Time.deltaTime);
 
+		if (Input.GetKey("left ctrl"))
+		{
+			this.x += Input.GetAxis("Horizontal") * 0.5f * PlayerPrefs.GetFloat("MovementSpeed", 1f);
+			this.y += Input.GetAxis("Vertical") * 0.5f * PlayerPrefs.GetFloat("MovementSpeed", 1f);
+		}
+		else
+		{
+			this.x += Input.GetAxis("Horizontal") * 0.2f * PlayerPrefs.GetFloat("MovementSpeed", 1f);
+			this.y += Input.GetAxis("Vertical") * 0.2f * PlayerPrefs.GetFloat("MovementSpeed", 1f);
+		}
+		base.transform.position = new Vector3(this.x, this.y, -10f);
+		base.GetComponent<Camera>().orthographicSize = this.orthographicSize;
+	}
 
+	private void Zoom(float amount)
+	{
+		var camera = this.GetComponent<Camera>();
 
+		this.orthographicSize = camera.orthographicSize;
 
-        if (Input.GetKey("left ctrl"))
-        {
-            this.x += Input.GetAxis("Horizontal") * 0.5f * PlayerPrefs.GetFloat("MovementSpeed", 1f);
-            this.y += Input.GetAxis("Vertical") * 0.5f * PlayerPrefs.GetFloat("MovementSpeed", 1f);
-        }
-        else
-        {
-            this.x += Input.GetAxis("Horizontal") * 0.2f * PlayerPrefs.GetFloat("MovementSpeed", 1f);
-            this.y += Input.GetAxis("Vertical") * 0.2f * PlayerPrefs.GetFloat("MovementSpeed", 1f);
-        }
-        base.transform.position = new Vector3(this.x, this.y, -10f);
-        base.GetComponent<Camera>().orthographicSize = this.orthographicSize;
-    }
+		var camSize = new Vector2(this.orthographicSize * 2 * camera.aspect, this.orthographicSize * 2);
 
-    public static CameraPan instance;
+		var addSize = camSize*amount;
+
+		this.orthographicSize += addSize.y;
+		if (this.orthographicSize < 0.5)
+			this.orthographicSize = 0.5f;
+		else
+		{
+			if (amount != 0)
+			{
+
+			}
+		}
+	}
+
+	public static CameraPan instance;
 }
