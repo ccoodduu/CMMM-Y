116afbda46ceea2f3005c4bc2f55cf1a59ca6933
ccoodduu <william@guldagermadsen.dk>
2023-03-06 22:09:07 +0100

    Deleted launcher folder?

--- launcher/src/components/ProgressBar.js
+++
@@ -1,8 +0,0 @@
-export const ProgressBar = ({ id }) => (
-  <div className="pg-bar-container">
-    <div className={`pg-bar-text ${id}-text`}>Downloading (0%)...</div>
-    <div className="pg-bar-inner">
-      <div className={`pg-bar ${id}`}></div>
-    </div>
-  </div>
-);
