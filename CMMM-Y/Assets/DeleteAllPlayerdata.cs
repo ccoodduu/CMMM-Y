7b7d0fce17641e3ca14685e08704cbb392109bff
ccoodduu <william@guldagermadsen.dk>
2023-03-12 17:25:09 +0100

    Added DeleteAll button

---
+++ CMMM-Y/Assets/DeleteAllPlayerdata.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeleteAllPlayerdata : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void DeleteAll()
+    {
+        PlayerPrefs.DeleteAll();
+
+        // Reload scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
