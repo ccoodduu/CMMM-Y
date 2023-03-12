0844f26bdd4ada6d53a74c9d0b6c3a3376b0a43d
ccoodduu <william@guldagermadsen.dk>
2023-03-12 21:51:09 +0100

    Crop button hides in vault mode

---
+++ CMMM-Y/Assets/Scripts/UI/Level Scene/HideIfMode.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideIfMode : MonoBehaviour
+{
+    public Mode_e mode;
+    void Start()
+    {
+        if (GridManager.mode == mode)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
