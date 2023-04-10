29f09f85c005b5fb30d29551de317e23360ccba4
ccoodduu <william@guldagermadsen.dk>
2023-04-10 21:15:25 +0200

    Added sound preview for slider

--- CMMM-Y/Assets/Scripts/UI/Options/SettingsSlider.cs
+++ CMMM-Y/Assets/Scripts/UI/Options/SettingsSlider.cs
@@ -11,12 +11,27 @@ public class SettingsSlider : MonoBehaviour
 	public string sliderType;
 	public float deafaultSliderValue;
 
+	public AudioClip previewAudioClip;
+
+	private AudioSource audioSource;
+	private bool awake;
+
 	void Start()
 	{
 		var value = PlayerPrefs.GetFloat(playerPrefString, deafaultSliderValue);
 
+		if (previewAudioClip != null)
+		{
+			audioSource = gameObject.AddComponent<AudioSource>();
+			audioSource.loop = false;
+			audioSource.playOnAwake = false;
+			audioSource.volume = 0f;
+			audioSource.clip = previewAudioClip;
+		}
+
 		GetComponent<Slider>().value = value;
 		UpdateSlider(value);
+		awake = true;
 	}
 
 	public void UpdateSlider(float val)
@@ -25,7 +40,16 @@ public class SettingsSlider : MonoBehaviour
 
 		if (playerPrefString == "Music Volume") MusicManager.instance.UpdateVolume();
 
-		if (sliderType == "Volume") sliderValueText.text = Mathf.Round(val * 100f) + "%";
+		if (sliderType == "Volume")
+		{
+			sliderValueText.text = Mathf.Round(val * 100f) + "%";
+
+			if (awake && previewAudioClip != null) 
+			{ 
+				audioSource.volume = val; 
+				if (!audioSource.isPlaying) audioSource.Play(); 
+			}
+		}
 		if (sliderType == "Speed") sliderValueText.text = Mathf.Round(val * 10f) / 10f + "x";
 	}
 }
